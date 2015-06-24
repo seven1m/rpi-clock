24f075c1fcfb93ef0b4454a051aaa58440f6c22c
Tim Morgan <tim@timmorgan.org>
2015-06-23 21:46:35 -0500

    Use constants for lat, lon, and units

--- app.rb
+++ app.rb
@@ -6,14 +6,19 @@ require 'erb'
 require 'time'
 require 'json'
 
+LAT = 36.008111
+LON = -95.942436
+UNITS = 'imperial'
+
 get '/' do
   @now = Time.now
   tries = 0
   begin
     tries += 1
-    weather_json = open('http://api.openweathermap.org/data/2.5/weather?lat=36.008111&lon=-95.942436&units=imperial').read
-    forecast_json = open('http://api.openweathermap.org/data/2.5/forecast/daily?lat=36.008111&lon=-95.942436&cnt=1&mode=json&units=imperial').read
+    weather_json = open("http://api.openweathermap.org/data/2.5/weather?lat=#{LAT}&lon=#{LON}&units=#{UNITS}").read
+    forecast_json = open("http://api.openweathermap.org/data/2.5/forecast/daily?lat=#{LAT}&lon=#{LON}&cnt=1&mode=json&units=#{UNITS}").read
   rescue
+    # sometimes the api returns bad data :-/
     sleep 5
     retry unless tries > 60 # 5 minutes
   end
